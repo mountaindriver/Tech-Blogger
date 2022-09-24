ed00d29d76590f4a5956bf904d69cb1af6ed99c1
Mountian Driver <lsfreigenberg@outlook.com>
2022-09-24 15:35:21 -0600

    create homepage.handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -25,7 +25,7 @@ router.get('/', async (req, res)=> {
             logged_in: req.session.logged_in
         });
     } catch (err){
-        res.status(500).json(err);
+        res.status(500).json({message: "hello there"});
     }
 });
 
