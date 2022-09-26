9a73f6535cdd4f897ea1ba2dd5fee65422d24d7b
Mountian Driver <lsfreigenberg@outlook.com>
2022-09-25 19:17:40 -0600

    add log out funtion to button

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -39,8 +39,13 @@ router.get('/', async (req, res) => {
 });
 
 // gets a specific blog post
-router.get('/post/:id', async (req, res) => {
+router.get('/post/:id',withAuth, async (req, res) => {
     try {
+        if (!req.session.logged_in) {
+            res.redirect('/login');
+            return;
+        }
+
         const blogData = await Blog.findByPk(req.params.id, {
             include: [
                 {
@@ -72,6 +77,10 @@ router.get('/post/:id', async (req, res) => {
 // Use withAuth middleware to prevent access to route
 router.get('/dashboard', withAuth, async (req, res) => {
     try {
+        if (!req.session.logged_in) {
+            res.redirect('/');
+            return;
+        }
         // find the logged in user with session ID
         const userData = await User.findByPk(req.session.user_id, {
             attributes: { exlude: ['password'] },
@@ -81,9 +90,10 @@ router.get('/dashboard', withAuth, async (req, res) => {
         const user = userData.get({ plain: true });
 
         res.render('dashboard', {
-            ...user,
+            user,
             logged_in: true
         });
+
     } catch (err) {
         res.status(500).json(err);
     }
@@ -92,11 +102,10 @@ router.get('/dashboard', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/');
-      return;
+        res.redirect('/');
+        return;
     }
-  
     res.render('login');
-  });
+});
 
 module.exports = router;
